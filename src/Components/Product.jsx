6770d8bd499691f5e64096306eaa210893294b96
Sa3d0o <105570840+Sa3dMustafa@users.noreply.github.com>
2023-10-20 01:20:20 +0300

    commit 2

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
+
 function Product(props) {
-    const { product,showButton } = props;
+    const { product, showButton, hideDetails } = props;
+    const location = useLocation();
+
+    // Define the description to be displayed based on the route
+    const descriptionToShow = location.pathname === "/" ? product.description.substring(0, 100) : product.description;
 
     return (
-        <div className="card mb-3 shadow-lg">
+        <div className="card mb-3 shadow-lg d-flex align-items-center">
             <img src={product.image} className="card-img-top height-200 p-4" alt={product.title} />
             <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <p className="card-text min-h">{product.description}</p>
-                    <p>Price : {product.price}$</p>
-                    {showButton && <Link to={`/product/${product.id}`} className="btn btn-primary">Detailes</Link>}
+                <p className="card-text">{descriptionToShow} {location.pathname === "/" && "...."}</p>
+                <p>Price: {product.price}$</p>
+                {showButton && (
+                    <Link to={`/product/${product.id}`} className="btn btn-primary">
+                        Details
+                    </Link>
+                )}
             </div>
         </div>
     );
--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,25 +1,58 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Product from "./Product";
 
 function ProductsList() {
   const apiUrl = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const getAllProducts = () => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
+
+  const getCategories = () => {
+    fetch(`${apiUrl}/categories`)
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  };
+
+  const getProductsByCategory = (catName) => {
+    fetch(`${apiUrl}/category/${catName}`)
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  };
 
   useEffect(() => {
-    fetch(apiUrl)
-      .then((response) => response.json()) // Corrected the response.json() function call
-      .then((data) => setProducts(data));
+    getAllProducts();
+    getCategories();
   }, []);
 
   return (
     <>
       <h2 className="text-center p-3">Our Products</h2>
       <div className="container">
+        <div className="cat d-flex justify-content-center flex-wrap gap-md-4 mb-4">
+        <button onClick={()=>{
+          getAllProducts();
+        }} className="btn btn-info mb-2 me-2">All</button>
+          {categories.map((categorie) => (
+            <button
+              key={categorie}
+              onClick={() => {
+                getProductsByCategory(categorie);
+              }}
+              className="btn btn-info mb-2 me-2"
+            >
+              {categorie}
+            </button>
+          ))}
+        </div>
         <div className="row">
           {products.map((product) => (
-            <div className="col-sm-12 col-md-6 col-lg-3 " key={product.id}>
-              <Product product={product} showButton={true} />{" "}
-              {/* Pass the individual product to the Product component */}
+            <div className="col-sm-12 col-md-6 col-lg-3" key={product.id}>
+              <Product product={product} showButton={true} hiddeDetiels={false}/>
             </div>
           ))}
         </div>
